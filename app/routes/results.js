f3f0b4da153c752f54f192250de9161c6b88861b
fabischapeli <fabischapeli97@gmail.com>
2017-11-21 18:13:59 +0300

    implementing ember services

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?zip=' + params.zip + 'apikey=0613f3c5dde44d699a1a8c7adb2e6ed7';
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' + key + '&zip=' + params.zip;
     return Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
     });
